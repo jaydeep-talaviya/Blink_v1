2bb1eef4314b852668d9979ea84eb6f074d39f04
jaydeep <jaydeeptalaviya8@gmail.com>
2023-09-25 21:13:08 +0530

    add template tag to get product attribute id from orderlines product

--- staffs/templatetags/attribute_from_orderline.py
+++ staffs/templatetags/attribute_from_orderline.py
@@ -1,6 +1,6 @@
 from django import template
 
-from utils.helper_functions import get_attribute_full_name
+from utils.helper_functions import get_attribute_full_name, get_warehouse_dict
 
 register = template.Library()
 
@@ -8,3 +8,29 @@ register = template.Library()
 @register.filter(name='get_attribute_from_orderline')
 def get_attribute_from_orderline(record):
     return get_attribute_full_name(record)
+
+
+@register.filter(name='get_attribute_id_from_orderline')
+def get_attribute_id_from_orderline(record):
+    # List of attribute values you want to filter with
+    attribute_values_list = record.selected_product_varient.split(',')[:-1]
+    # Initialize a list to hold the querysets for each attribute value
+    querysets = []
+
+    # Create a queryset for each attribute value
+    for attr_value in attribute_values_list:
+        queryset = record.product_id.productchangepriceattributes_set.filter(attribute_values__a_value=attr_value)
+        querysets.append(queryset)
+
+    # Find the intersection of the querysets
+    filtered_records = querysets[0]
+    for queryset in querysets[1:]:
+        filtered_records = filtered_records.intersection(queryset)
+    print(">>>>>>>>>>intersection\n\n\n\n",filtered_records)
+    return filtered_records.first().id
+
+
+@register.filter(name='get_warehouse_from_orderline')
+def get_warehouse_from_orderline(record):
+    return get_warehouse_dict(record)
+
