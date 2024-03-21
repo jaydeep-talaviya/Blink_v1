e49dc54cf5397aa5a031fc23197b9953fd87a0fc
jaydeep <jaydeeptalaviya8@gmail.com>
2024-03-21 21:20:11 +0530

    added changes for deployments

--- products/templatetags/custom_tags.py
+++ products/templatetags/custom_tags.py
@@ -6,7 +6,6 @@ register = template.Library()
 # @register.simple_tag
 @register.filter(name='get_avarage_rate')
 def get_avarage_rate(value):
-    print(value.count(),".....................")
     if value.count() != 0:
         avg_rates=value.aggregate(Avg('rate'))['rate__avg']
         rate_list=['orange' for i in range(int(avg_rates))]+['black' for i in range(5-int(avg_rates))]
