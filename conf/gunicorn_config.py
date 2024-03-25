33f6fd09be716111cbd2bc375e819cc8f4831312
jaydeep <jaydeeptalaviya8@gmail.com>
2024-03-25 11:30:08 +0530

    added changes for media folder uploading exist images

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/media/jay/New Volume/development_2022_2023/Blink-main/venv/bin/gunicorn'
+pythonpath = '/media/jay/New Volume/development_2022_2023/Blink-main'
+bind = '127.0.0.1:8000'
+workers = 3
